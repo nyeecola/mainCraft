0d5bd68d6269b96d241c3bf162fcfa0088e5a4b2
Igor Matheus Andrade Torrente <igormtorrente@gmail.com>
2020-12-17 22:01:28 -0300

    Vulkan: Add create_instance
    Add a function to fill the application info and create a vulkan instance.
    
    Signed-off-by: Igor Matheus Andrade Torrente <igormtorrente@gmail.com>

--- vulkan/vk_backend.h
+++ vulkan/vk_backend.h
@@ -1,6 +1,15 @@
 #ifndef VK_BACKEND_H
 #define VK_BACKEND_H
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+struct vk_program {
+	VkApplicationInfo app_info;
+	VkInstance instance;
+};
+
 
 int
 run_vk(const int argc, char *const *argv);
