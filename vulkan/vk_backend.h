3d8bc15201a3353c8484896e9210e323f5f9b7fb
Igor Matheus Andrade Torrente <igormtorrente@gmail.com>
2020-12-19 16:21:07 -0300

    Vulkan: Implement a algorithm to select a suitable GPU(device)
    Implements a simple algorithm to select a GPU. Querying the GPU supported
    families queues, extensions support(Swap chain support). Also adds fields
    for vk_program structure to hold these new pieces of information about the
    GPU.
    
    Signed-off-by: Igor Matheus Andrade Torrente <igormtorrente@gmail.com>

--- vulkan/vk_backend.h
+++ vulkan/vk_backend.h
@@ -5,11 +5,26 @@
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
+
+enum family_indices { graphics = 0, transfer, compute, protectedBit, sparseBindingBit, present, queues_count };
+
+struct vk_queues {
+	uint32_t family_indices[queues_count];
+	uint32_t queue_count[queues_count];
+};
+
+struct vk_device {
+	VkPhysicalDevice physical_device;
+	struct vk_queues queues;
+};
+
+
 struct vk_program {
 	GLFWwindow *window;
 	VkSurfaceKHR surface;
 	VkApplicationInfo app_info;
 	VkInstance instance;
+	struct vk_device device;
 };
 
 
