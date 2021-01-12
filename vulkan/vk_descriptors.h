cc7bb82e6d0800ed756460220dfe1ee326a2282e
Igor Matheus Andrade Torrente <igormtorrente@gmail.com>
2021-01-12 15:35:36 -0300

    Vulkan: Improve the binding and attribute descriptors
    Removes the dynamic allocation of these functions, which lead to one less
    point of failure in the create pipeline function.
    
    Signed-off-by: Igor Matheus Andrade Torrente <igormtorrente@gmail.com>

--- vulkan/vk_descriptors.h
+++ vulkan/vk_descriptors.h
@@ -5,11 +5,12 @@
 
 #include "vk_types.h"
 
-VkVertexInputBindingDescription
-get_vertex_binding_description(uint32_t binding);
+void
+get_vertex_binding_description(uint32_t binding, VkVertexInputBindingDescription *binding_description);
 
-VkVertexInputAttributeDescription *
-get_vertex_attribute_descriptions(uint32_t binding, uint32_t first_location);
+void
+get_vertex_attribute_descriptions(uint32_t binding, uint32_t first_location,
+								  VkVertexInputAttributeDescription *attribute_descriptions);
 
 VkDescriptorSetLayout
 create_descriptor_set_layout_binding(VkDevice logical_device);
